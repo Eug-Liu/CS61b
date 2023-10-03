ebcffcd1d3352e1aead783dbceba2fa82168579d
Chenhe Liu <dvince672@gmail.com>
2023-10-03 22:36:02 +1100

    proj1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,28 +1,47 @@
-public class ArrayDeque<T>{
+/** second part of project1A.
+ * deque implemented by array
+ * @author FlyingPig
+ */
+public class ArrayDeque<T> {
 
-    private T items[];
+    /** array to save data.*/
+    private T[] array;
+    /** size of the deque. */
     private int size;
-    private int nextFirst;
-    private int nextLast;
+
+    /** size of the array. */
     private int length;
 
-    /** Creates an empty list. */
+    /** front index. */
+    private int front;
+
+    /** last index. */
+    private int last;
+
+    /** constructor for ArrayDeque. */
     public ArrayDeque() {
-        items = (T[]) new Object[8];
+        array = (T[]) new Object[8];
         size = 0;
-        nextFirst = 7;
-        nextLast = 0;
         length = 8;
+        front = 4;
+        last = 4;
     }
 
-    public boolean isEmpty(){
+    /** decide if the deque is empty.
+     * @return true if the deque is empty, vice versa.
+     */
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public int size(){
+    /** return the size of the deque. */
+    public int size() {
         return size;
     }
 
+    /** return the "index - 1".
+     * @param index index
+     */
     private int minusOne(int index) {
         if (index == 0) {
             return length - 1;
@@ -30,115 +49,123 @@ public class ArrayDeque<T>{
         return index - 1;
     }
 
-    private int plusOne(int index) {
-        index += 1;
-        if (index == length) {
+    /** return the "index + 1".
+     * @param index index
+     */
+    private int plusOne(int index, int module) {
+        index %= module;
+        if (index == module - 1) {
             return 0;
         }
-        return index;
+        return index + 1;
     }
 
-    public void grow(){
-
-        T[] a =(T[]) new Object[2 * length];
-        int first = plusOne(nextFirst);
-        int last = minusOne(nextLast);
-        if(first > last){
-            System.arraycopy(items, first, a, 0, length - first);
-            System.arraycopy(items, 0, a, length - first, last + 1);
-        } else{
-            System.arraycopy(items, first, a, 0, size);
+    private void grow() {
+        T[] newArray = (T[]) new Object[length * 2];
+        int ptr1 = front;
+        int ptr2 = length;
+        while (ptr1 != last) {
+            newArray[ptr2] = array[ptr1];
+            ptr1 = plusOne(ptr1, length);
+            ptr2 = plusOne(ptr2, length * 2);
         }
-        
+        front = length;
+        last = ptr2;
+        array = newArray;
         length *= 2;
-        items = a;
-        nextFirst = length - 1;
-        nextLast = size;
-
     }
 
-    public void shirnk(){
-
-        T[] a =(T[]) new Object[length / 2];
-        int first = plusOne(nextFirst);
-        int last = minusOne(nextLast);
-        if(first > last){
-            System.arraycopy(items, first, a, 0, length - first);
-            System.arraycopy(items, 0, a, length - first, last + 1);
-        } else{
-            System.arraycopy(items, first, a, 0, size);
+    private void shrink() {
+        T[] newArray = (T[]) new Object[length / 2];
+        int ptr1 = front;
+        int ptr2 = length / 4;
+        while (ptr1 != last) {
+            newArray[ptr2] = array[ptr1];
+            ptr1 = plusOne(ptr1, length);
+            ptr2 = plusOne(ptr2, length / 2);
         }
-        
+        front = length / 4;
+        last = ptr2;
+        array = newArray;
         length /= 2;
-        items = a;
-        nextFirst = length - 1;
-        nextLast = size;
     }
 
-    /** Inserts X into the back of the list. */
-    public void addLast(T x) {
-        if(size == length)
+    /** add one item at the front of the deque.
+     * @param item the item we want to add
+     */
+    public void addFirst(T item) {
+        if (size == length - 1) {
             grow();
-        items[nextLast] = x;
-        nextLast = plusOne(nextLast);
+        }
+        front = minusOne(front);
+        array[front] = item;
         size++;
     }
 
-    public void addFirst(T x){
-        if(size == length)
+    /** add one item at the end of the deque.
+     * @param item item we want to add
+     */
+    public void addLast(T item) {
+        if (size == length - 1) {
             grow();
-        items[nextFirst] = x;
-        nextFirst = minusOne(nextFirst);
+        }
+        array[last] = item;
+        last = plusOne(last, length);
         size++;
     }
 
-    /** Deletes item from back of the list and
-      * returns deleted item. */
+    /** remove the first item.
+     * @return the removed first item
+    */
+    public T removeFirst() {
+        if (length >= 16 && length / size >= 4) {
+            shrink();
+        }
+        if (size == 0) {
+            return null;
+        }
+        T ret = array[front];
+        front = plusOne(front, length);
+        size--;
+        return ret;
+    }
+
+    /** remove the last item.
+     * @return the removed last item
+     */
     public T removeLast() {
-        if(size == 0)
+        if (length >= 16 && length / size >= 4) {
+            shrink();
+        }
+        if (size == 0) {
             return null;
-        int last = minusOne(nextLast);
-        T x = items[last];
-        nextLast = last;
+        }
+        last = minusOne(last);
         size--;
-        if(size * 4 < length)
-            shirnk();
-        return x;
+        return array[last];
     }
 
-    public T removeFirst(){
-        if(size == 0)
+    /** return the item indexed at index.
+     * @param index index
+     */
+    public T get(int index) {
+        if (index >= size) {
             return null;
-        int first = plusOne(nextFirst);
-        T x = items[first];
-        nextFirst = first;
-        size--;
-        if(size * 4 < length)
-            shirnk();
-        return x;
+        }
+        int ptr = front;
+        for (int i = 0; i < index; i++) {
+            ptr = plusOne(ptr, length);
+        }
+        return array[ptr];
     }
 
-    public T get(int index){
-        if(index >= size)
-            return null;
-        int first = plusOne(nextFirst);
-        int last = minusOne(nextLast);
-        if(first < last)
-            return items[first + index];
-        else{
-            if(index <= length - first - 1)
-                return items[first + index];
-            else return items[index - length + first];
+    /** print the entire deque from front to end. */
+    public void printDeque() {
+        int ptr = front;
+        while (ptr != last) {
+            System.out.print(array[ptr] + " ");
+            ptr = plusOne(ptr, length);
         }
     }
 
-    public void printDeque(){
-        int first = plusOne(nextFirst);
-        int index = first;
-        for(int rest = size; rest > 0; rest--){
-            System.out.print(items[index] + " ");
-            index = plusOne(index);
-        }       
-        System.out.println("");
-    }
-} 
+}
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,82 +1,87 @@
-public class LinkedListDeque<T>{
-    
-    private class Node{
+public class LinkedListDeque<T> {
+
+    private class Node {
         public T item;
         public Node next;
         public Node pre;
 
-        public Node(T i, Node p, Node n){
+        public Node(T i, Node p, Node n) {
             item = i;
             pre = p;
             next = n;
         }
 
-        public Node(){}
-            
-    }         
-    
+        public Node() {
+        }
+
+    }
+
     private Node sentinel;
     private int size;
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public LinkedListDeque(){
+    public LinkedListDeque() {
         sentinel = new Node();
         sentinel.next = sentinel;
         sentinel.pre = sentinel;
-        size = 0; 
+        size = 0;
     }
 
-    public void addFirst(T i){
+    public void addFirst(T i) {
         Node first = new Node(i, sentinel, sentinel.next);
-        first.next.pre = first;
+        sentinel.next.pre = first;
         sentinel.next = first;
-        size ++;
+        size++;
     }
 
-    public void addLast(T i){
+    public void addLast(T i) {
         Node last = new Node(i, sentinel.pre, sentinel);
-        last.pre.next = last;
+        sentinel.pre.next = last;
         sentinel.pre = last;
-        size ++;
+        size++;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public void printDeque(){
+    public void printDeque() {
         Node p = sentinel.next;
-        for(int i = size; i != 0; i--){
+        for (int i = size; i != 0; i--) {
             System.out.print(p.item + " ");
             p = p.next;
         }
-        System.out.println("");
+        System.out.println();
     }
 
-    public T removeFirst(){
-        if(size == 0)
+    public T removeFirst() {
+        if (size == 0)
             return null;
         T first = sentinel.next.item;
+        sentinel.next.next.pre = sentinel;
         sentinel.next = sentinel.next.next;
         size--;
-        return first;      
+        return first;
     }
 
-    public T removeLast(){
-        if(size == 0)
+    public T removeLast() {
+        if (size == 0)
             return null;
         T last = sentinel.pre.item;
+        sentinel.pre.pre.next = sentinel;
         sentinel.pre = sentinel.pre.pre;
         size--;
-        return last;      
+        return last;
     }
 
-    public T get(int index){
+    public T get(int index) {
+        if(index >= size)
+            return null;
         Node p = sentinel.next;
-        while(index != 0){
+        while (index != 0) {
             p = p.next;
             index--;
         }
@@ -84,14 +89,14 @@ public class LinkedListDeque<T>{
         return p.item;
     }
 
-    public T RecursiveHelp(int index, Node start){
-        if(index == 0)
+    public T RecursiveHelp(int index, Node start) {
+        if (index == 0)
             return start.item;
-        
-        return RecursiveHelp(index-1, start.next);
+
+        return RecursiveHelp(index - 1, start.next);
     }
 
-    public T getRecursive(int index){
+    public T getRecursive(int index) {
         return RecursiveHelp(index, sentinel.next);
     }
 
