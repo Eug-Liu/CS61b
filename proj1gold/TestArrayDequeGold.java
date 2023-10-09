96cc2143edc84a31dce20e841f7adedf771cfa04
Chenhe Liu <dvince672@gmail.com>
2023-10-10 07:31:31 +1100

    damnman

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -9,7 +9,7 @@ public class TestArrayDequeGold {
         ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
         double method;
         String log = "";
-        for(int times = 10000; times > 0; times--){
+        for(int times = 1000; times > 0; times--){
             method = StdRandom.uniform();
             if (method <= 0.25){
                 student.addFirst(times);
@@ -21,15 +21,15 @@ public class TestArrayDequeGold {
                 log += "addLast(" + times + ")\n";
             } else if (method <= 0.75) {
                 if (!student.isEmpty()){
-                    int get = student.removeFirst();
-                    int exp = solution.removeFirst();
+                    Integer get = student.removeFirst();
+                    Integer exp = solution.removeFirst();
                     log += "removeFirst(): " + get + "\n";
                     assertEquals(log, exp, get);
                 }
             } else{
                 if (!student.isEmpty()){
-                    int get = student.removeLast();
-                    int exp = solution.removeLast();
+                    Integer get = student.removeLast();
+                    Integer exp = solution.removeLast();
                     log += "removeLast(): " + get + "\n";
                     assertEquals(log, exp, get);
                 }
